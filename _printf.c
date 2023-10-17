6314f635e851326ae0b7950579ab97c4282d8931
Maysoon-Magdy <MaysoonMagdy@icloud.com>
2023-10-17 14:10:59 -0700

    fix betty errors in _printf.c

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args_List;
-	int char_len , s_len= 0;
+	int char_len, s_len = 0;
 
 	if (format == NULL)
 		return (-1);
@@ -27,28 +27,26 @@ int _printf(const char *format, ...)
 			format++;
 				if (*format == '\0')
 					break;
-
 				if (*format == '%')
 				{
 					write(1, format, 1);
 					char_len++;
-				}	
+				}
 				else if (*format == 'c')
 				{
+					char c = va_arg(args_List, int);
 
-					char c= va_arg(args_List, int);
 					write(1, &c, 1);
 					char_len++;
 				}
 				else if (*format == 's')
 				{
+					char *s = va_arg(args_List, char*);
 
-					char *s= va_arg(args_List, char*);
 					s_len = strlen(s);
 					write(1, s, s_len);
 					char_len += s_len;
 				}
-
 		}
 		format++;
 	}
