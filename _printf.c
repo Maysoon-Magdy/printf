acd739f5011030cfdd39734f1bdcb5ada80fd224
Maysoon-Magdy <MaysoonMagdy@icloud.com>
2023-10-17 14:41:10 -0700

    _printf.c refinment

--- _printf.c
+++ _printf.c
@@ -1,19 +1,14 @@
 #include "main.h"
 /**
- * _printf - a funtion that prints the given values
- * @format: string to be printed
+ * define_printf_format - Afunction to defint the format string
+ * @format: the format string
+ * @args_List: the list of arguments
  *
- * Return: the length of printed string
+ * Return: number of char
  */
-int _printf(const char *format, ...)
+int define_printf_format(const char *format, va_list args_List)
 {
-	va_list args_List;
-	int char_len, s_len = 0;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(args_List, format);
+	int char_len, s_len;
 
 	while (*format)
 	{
@@ -25,33 +20,51 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
-				if (*format == '\0')
-					break;
-				if (*format == '%')
-				{
-					write(1, format, 1);
-					char_len++;
-				}
-				else if (*format == 'c')
-				{
-					char c = va_arg(args_List, int);
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				char_len++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args_List, int);
 
-					write(1, &c, 1);
-					char_len++;
-				}
-				else if (*format == 's')
-				{
-					char *s = va_arg(args_List, char*);
+				write(1, &c, 1);
+				char_len++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args_List, char*);
 
-					s_len = strlen(s);
-					write(1, s, s_len);
-					char_len += s_len;
-				}
+				s_len = strlen(s);
+				write(1, s, s_len);
+				char_len += s_len;
+			}
 		}
 		format++;
 	}
-
-	va_end(args_List);
 	return (char_len);
+}
+
+/**
+ * _printf - a funtion that prints the given values
+ * @format: string to be printed
+ *
+ * Return: the length of printed string
+ */
+int _printf(const char *format, ...)
+{
+	va_list args_List;
+	int char_ret = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args_List, format);
+	char_ret = define_printf_format(format, args_List);
+	va_end(args_List);
+	return (char_ret);
 }
 
